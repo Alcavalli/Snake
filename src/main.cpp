14a07c5e3302998164ef33e4e6ed719cfc57467c
Alcavalli <alcavalli09@gmail.com>
2026-05-26 01:38:31 +0200

    pulizia del main

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,6 @@ int main()
 {
     Game partita;
     partita.run();
-    /*
-    while (window.isOpen())
-    {
-        // 1. Input
-        sf::Event event;
-        while (window.pollEvent(event))
-            if (event.type == sf::Event::Closed)
-                window.close();
-
-        // 2. Update
-        // logica gioco
-
-        // 3. Render
-        window.clear();
-        // disegna cose
-        window.display();
-    }*/
 
     return 0;
 }
