48aff6c0e73e0c7a5526c49e43ab91acec9cb198
Alcavalli <alcavalli09@gmail.com>
2026-05-26 01:21:34 +0200

    implementazione processamento input

--- src/Game.cpp
+++ src/Game.cpp
@@ -17,6 +17,13 @@ void Game::run()
     }
 }
 
-void Game::processInput() {}
+void Game::processInput()
+{
+    sf::Event event;
+    while (window.pollEvent(event))
+        if (event.type == sf::Event::Closed)
+            window.close();
+}
+
 void Game::update() {}
 void Game::render() {}
