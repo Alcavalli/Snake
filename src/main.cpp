051b258396595016f3bce3a11f586d2819e5acb1
Alcavalli <alcavalli09@gmail.com>
2026-05-26 00:16:44 +0200

    configurazione SFML nel main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
+#include <SFML/Graphics.hpp>
+
 int main()
 {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Titolo");
 
+    while (window.isOpen())
+    {
+        // 1. Input
+        sf::Event event;
+        while (window.pollEvent(event))
+            if (event.type == sf::Event::Closed)
+                window.close();
+
+        // 2. Update
+        // logica gioco
+
+        // 3. Render
+        window.clear();
+        // disegna cose
+        window.display();
+    }
 
     return 0;
 }
