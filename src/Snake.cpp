aaebe20dd7824a0a1221720d29c92e077715dc16
Alcavalli <alcavalli09@gmail.com>
2026-05-31 09:29:38 +0200

    inizio implementazione sorgente snake

--- src/Snake.cpp
+++ src/Snake.cpp
@@ -6,5 +6,22 @@ void Snake::cambiaDir(Direzione nuova_dir) { this->dir = nuova_dir; }
 
 void Snake::aggiornaCorpo()
 {
-
+    sf::Vector2i temp = corpo.back();
+    switch(dir)
+    {
+        case Direzione::Su:
+            --temp.y;           // con SFML y cresce andando verso il basso e sale verso l'alto
+            break;
+        case Direzione::Giu:
+            ++temp.y;
+            break;
+        case Direzione::Destra:
+            ++temp.x;
+            break;
+        case Direzione::Sinistra:
+            --temp.x;
+            break;
+    }
+    corpo.push_back(temp);
+    corpo.pop_front();
 }
